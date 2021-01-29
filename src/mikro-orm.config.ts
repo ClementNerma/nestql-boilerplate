ac819474c989bc911e013c4a9eed3d785a4a9ed3
Clément Nerma <clement.nerma@gmail.com>
2021-01-29 21:39:25 +0100

    Default the database to PostgreSQL

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,8 +3,11 @@ import { MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs'
 const options: MikroOrmModuleSyncOptions = {
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
-  dbName: 'mango.sqlite3',
-  type: 'sqlite',
+  type: 'postgresql',
+  host: process.env['DB_HOST'] ?? 'localhost',
+  dbName: process.env['DB_NAME'] ?? 'mango',
+  user: process.env['DB_USERNAME'] ?? 'mango',
+  password: process.env['DB_PASSWORD'] ?? 'mango',
 }
 
 export default options
