5ff992171775adff1b657059f42a4eb4ea21a47b
Clément Nerma <clement.nerma@gmail.com>
2021-01-30 13:52:54 +0100

    Add support for .env and .env.dev files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import { config as dotenv } from 'dotenv'
 import { install as installSourceMap } from 'source-map-support'
 import { AppModule } from './app.module'
 import { setupEnv } from './tscore'
 
 installSourceMap()
+
+dotenv({ path: '.env' })
+dotenv({ path: '.env.dev' })
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
